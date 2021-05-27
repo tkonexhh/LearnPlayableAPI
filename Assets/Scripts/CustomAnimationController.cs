edb0fdbc0bc42d1dbfac60b82cc841a502228cae
neo.xu <neo.xu@modooplay.com>
2021-05-27 21:05:13 +0800

    ...

--- Assets/Scripts/CustomAnimationController.cs
+++ Assets/Scripts/CustomAnimationController.cs
@@ -2,61 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.Audio;
 using UnityEngine.Playables;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class CustomAnimationController : MonoBehaviour
 {
-    public AnimationClip clip;
-    public AnimationClip clip2;
-
+    // public AnimationClip[] clipsToPlay;
+    public AnimationClip animationClip;
+    public AudioClip audioClip;
 
     private PlayableGraph m_Graph;
-    private AnimationPlayableOutput m_Output;
-    private AnimationMixerPlayable m_Mixer;
 
 
-    public float tranTime = 5;
-    private float leftTime;
 
     void Start()
     {
-        leftTime = tranTime;
-
-
         m_Graph = PlayableGraph.Create("CustomAnimationController");
-        GraphVisualizerClient.Show(m_Graph);
         m_Graph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+        GraphVisualizerClient.Show(m_Graph);
 
-        m_Output = AnimationPlayableOutput.Create(m_Graph, "Animation", GetComponent<Animator>());
 
-        // AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(m_Graph, clip);
-        // m_Output.SetSourcePlayable(clipPlayable);
-        // m_Graph.Play();
+        // AnimationPlayableOutput animOutput = AnimationPlayableOutput.Create(m_Graph, "Animation", GetComponent<Animator>());
+        // var queuePlayableBehaviour = ScriptPlayable<CustomPlayableBehaviour>.Create(m_Graph);
+        // var playQueue = queuePlayableBehaviour.GetBehaviour();
+        // playQueue.Init(clipsToPlay, queuePlayableBehaviour, m_Graph);
+        // animOutput.SetSourcePlayable(queuePlayableBehaviour, 0);
 
-        m_Mixer = AnimationMixerPlayable.Create(m_Graph, 2);
-        m_Output.SetSourcePlayable(m_Mixer);
+        AnimationPlayableOutput animOutput = AnimationPlayableOutput.Create(m_Graph, "Animation Output", GetComponent<Animator>());
+        var animationPlayable = AnimationClipPlayable.Create(m_Graph, animationClip);
+        animOutput.SetSourcePlayable(animationPlayable, 0);
 
-        AnimationClipPlayable clipPlayable1 = AnimationClipPlayable.Create(m_Graph, clip);
-        AnimationClipPlayable clipPlayable2 = AnimationClipPlayable.Create(m_Graph, clip2);
-        m_Graph.Connect(clipPlayable1, 0, m_Mixer, 0);
-        m_Graph.Connect(clipPlayable2, 0, m_Mixer, 1);
-        m_Mixer.SetInputWeight(0, 1);
-        m_Mixer.SetInputWeight(1, 0);
+        AudioPlayableOutput audioOutput = AudioPlayableOutput.Create(m_Graph, "Audio", GetComponent<AudioSource>());
+        var audioPlayable = AudioClipPlayable.Create(m_Graph, audioClip, true);
+        audioOutput.SetSourcePlayable(audioPlayable, 0);
 
         m_Graph.Play();
     }
 
     void Update()
     {
-        if (leftTime > 0)
-        {
-            leftTime = leftTime - Time.deltaTime;
-            float weight = Mathf.Clamp(leftTime / tranTime, 0, 1);
-            Debug.LogError(weight);
-            m_Mixer.SetInputWeight(0, weight);
-            m_Mixer.SetInputWeight(1, 1 - weight);
-        }
     }
 
     private void OnDestroy()
--- Assets/Scripts/CustomPlayableBehaviour.cs
+++ Assets/Scripts/CustomPlayableBehaviour.cs
@@ -5,13 +5,14 @@ using UnityEngine.Animations;
 using UnityEngine.Playables;
 
 // A behaviour that is attached to a playable
+//顺序播放动画
 public class CustomPlayableBehaviour : PlayableBehaviour
 {
     private int m_CurrentClipIndex = -1;
     private float m_TimeToNextClip;
     private Playable m_Mixer;
 
-    public void Init(AnimationClip[] clipsToPlay, Playable owner, Playable graph)
+    public void Init(AnimationClip[] clipsToPlay, Playable owner, PlayableGraph graph)
     {
         owner.SetInputCount(1);
         m_Mixer = AnimationMixerPlayable.Create(graph, clipsToPlay.Length);
@@ -52,6 +53,27 @@ public class CustomPlayableBehaviour : PlayableBehaviour
     // Called each frame while the state is set to Play
     public override void PrepareFrame(Playable playable, FrameData info)
     {
-
+        if (m_Mixer.GetInputCount() == 0)
+            return;
+        // Advance to next clip if necessary
+        m_TimeToNextClip -= (float)info.deltaTime;
+        if (m_TimeToNextClip <= 0.0f)
+        {
+            m_CurrentClipIndex++;
+            if (m_CurrentClipIndex >= m_Mixer.GetInputCount())
+                m_CurrentClipIndex = 0;
+            var currentClip = (AnimationClipPlayable)m_Mixer.GetInput(m_CurrentClipIndex);
+            // Reset the time so that the next clip starts at the correct position
+            currentClip.SetTime(0);
+            m_TimeToNextClip = currentClip.GetAnimationClip().length;
+        }
+        // Adjust the weight of the inputs
+        for (int clipIndex = 0; clipIndex < m_Mixer.GetInputCount(); ++clipIndex)
+        {
+            if (clipIndex == m_CurrentClipIndex)
+                m_Mixer.SetInputWeight(clipIndex, 1.0f);
+            else
+                m_Mixer.SetInputWeight(clipIndex, 0.0f);
+        }
     }
 }
